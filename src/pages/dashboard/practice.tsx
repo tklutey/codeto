35be1576bd88255990744af3eabf04931973d2e2
tklutey <kluteyt@gmail.com>
2022-09-04 17:24:12 -0400

    Updating run button to mui component

--- src/pages/dashboard/practice.tsx
+++ src/pages/dashboard/practice.tsx
@@ -1,13 +1,15 @@
-import React, { ReactElement, useCallback, useRef } from 'react';
+import React, { ReactElement, useRef } from 'react';
 import Layout from 'layout';
 import MonacoEditor from '@monaco-editor/react';
+import { Button } from '@mui/material';
 
 const Practice = () => {
   const previewRef = useRef<any>(null);
   const entryFileValueRef = useRef<string | undefined>();
 
   const handleRunCode = () => {
-    previewRef.current?.getElementsByTagName('iframe')[0].contentWindow?.postMessage(
+    const rijuIframe = previewRef.current.getElementsByTagName('iframe')[0];
+    rijuIframe.contentWindow?.postMessage(
       {
         code: entryFileValueRef.current,
         event: 'runCode'
@@ -21,27 +23,30 @@ const Practice = () => {
   };
 
   return (
-    <div ref={previewRef} style={{ height: '100%', display: 'flex' }}>
-      <MonacoEditor
-        defaultLanguage="python"
-        loading={<div className="flex justify-center items-center w-full h-full font-bold text-white bg-bg-primary">Loading...</div>}
-        onChange={updateCode}
-        onMount={() => console.log('mounted')}
-        theme={'vs-dark'}
-        options={{
-          automaticLayout: true,
-          fontSize: '14px',
-          fontWeight: 600,
-          lineNumbersMinChars: 3,
-          minimap: { enabled: false },
-          scrollBeyondLastLine: false,
-          wordWrap: 'on'
-        }}
-        width="50%"
-        height="100%"
-      />
-      <iframe style={{ width: 'width%', height: '100%' }} src={`https://riju.codeamigo.xyz/python`} />
-      <button onClick={handleRunCode}>Run</button>
+    <div style={{ height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Button onClick={handleRunCode} style={{ height: '10%' }}>
+        Run Code
+      </Button>
+      <div ref={previewRef} style={{ height: '90%', width: '100%', display: 'flex' }}>
+        <MonacoEditor
+          defaultLanguage="javascript"
+          onChange={updateCode}
+          onMount={() => console.log('mounted')}
+          theme={'vs-dark'}
+          options={{
+            automaticLayout: true,
+            fontSize: '14px',
+            fontWeight: 600,
+            lineNumbersMinChars: 3,
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+            wordWrap: 'on'
+          }}
+          width="50%"
+          height="100%"
+        />
+        <iframe style={{ width: '50%', height: '100%' }} src={`https://riju.codeamigo.xyz/python`} />
+      </div>
     </div>
   );
 };
