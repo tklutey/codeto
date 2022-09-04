5edc43594d20986c9ea3030c30a766f2af0bb785
tklutey <kluteyt@gmail.com>
2022-09-04 16:52:26 -0400

    Adding runcode handler & button

--- src/pages/dashboard/practice.tsx
+++ src/pages/dashboard/practice.tsx
@@ -1,14 +1,31 @@
-import React, { ReactElement, useCallback } from 'react';
+import React, { ReactElement, useCallback, useRef } from 'react';
 import Layout from 'layout';
 import MonacoEditor from '@monaco-editor/react';
 
 const Practice = () => {
+  const previewRef = useRef<any>(null);
+  const entryFileValueRef = useRef<string | undefined>();
+
+  const handleRunCode = () => {
+    previewRef.current?.getElementsByTagName('iframe')[0].contentWindow?.postMessage(
+      {
+        code: entryFileValueRef.current,
+        event: 'runCode'
+      },
+      '*'
+    );
+  };
+
+  const updateCode = (newCode?: string, _?: any) => {
+    entryFileValueRef.current = newCode;
+  };
+
   return (
-    <div style={{ height: '100%', display: 'flex' }}>
+    <div ref={previewRef} style={{ height: '100%', display: 'flex' }}>
       <MonacoEditor
-        defaultLanguage="typescript"
+        defaultLanguage="python"
         loading={<div className="flex justify-center items-center w-full h-full font-bold text-white bg-bg-primary">Loading...</div>}
-        onChange={() => console.log('changed')}
+        onChange={updateCode}
         onMount={() => console.log('mounted')}
         theme={'vs-dark'}
         options={{
@@ -23,7 +40,8 @@ const Practice = () => {
         width="50%"
         height="100%"
       />
-      <iframe style={{ width: 'width%', height: '100%' }} src={`https://riju.codeamigo.xyz/java`} />
+      <iframe style={{ width: 'width%', height: '100%' }} src={`https://riju.codeamigo.xyz/python`} />
+      <button onClick={handleRunCode}>Run</button>
     </div>
   );
 };
