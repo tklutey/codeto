2e789f0d38cf7aea006de20d36b36f4a9f72b148
tklutey <kluteyt@gmail.com>
2022-10-02 19:02:59 -0500

    Adding fit addon to xterm

--- src/components/ide/CodeExecutionTerminal.tsx
+++ src/components/ide/CodeExecutionTerminal.tsx
@@ -1,20 +1,24 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
 import 'xterm/css/xterm.css';
 import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 
 const CodeExecutionTerminal = (props: Props) => {
   const { width, height, expectedOutput, terminalText } = props;
   const term = useRef<Terminal>();
   useEffect(() => {
     term.current = new Terminal();
+    const fitAddon = new FitAddon();
+    term.current.loadAddon(fitAddon);
     // @ts-ignore
     term.current.open(document.getElementById('terminal'));
+    fitAddon.fit();
   }, []);
 
   useEffect(() => {
     if (term.current && terminalText) {
       term.current.clear();
-      term.current.writeln(terminalText, () => console.log('wrote ' + terminalText));
+      term.current.write(terminalText, () => console.log('wrote ' + terminalText));
     }
   }, [terminalText]);
 
