e0c6104d47e98cb391fa5baa318f899f9a03a686
tklutey <kluteyt@gmail.com>
2022-10-13 15:34:44 -0700

    Fixing spinner

--- src/components/ide/RunButton.tsx
+++ src/components/ide/RunButton.tsx
@@ -1,6 +1,13 @@
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import { CircularProgress } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
+const Circle = styled('div')(({ theme }) => ({
+  width: '60px',
+  height: '60px',
+  borderRadius: '30px',
+  backgroundColor: theme.palette.primary.main
+}));
 const RunButton = (props: Props) => {
   const { run, isExecuting } = props;
   const handleClick = () => {
@@ -8,17 +15,17 @@ const RunButton = (props: Props) => {
   };
 
   return (
-    <div style={{ position: 'absolute', left: 'calc(50% - 30px)', top: '30%', zIndex: 10 }}>
-      <div onClick={handleClick} style={{ cursor: 'pointer' }} role="button">
+    <Circle style={{ position: 'absolute', left: 'calc(50% - 30px)', top: '30%', zIndex: 10 }}>
+      <div onClick={handleClick} style={{ cursor: 'pointer', width: '100%', height: '100%' }} role="button">
         {isExecuting ? (
-          <div>
-            <CircularProgress />
+          <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <CircularProgress color="inherit" />
           </div>
         ) : (
           <PlayCircleIcon sx={{ fontSize: 60 }} />
         )}
       </div>
-    </div>
+    </Circle>
   );
 };
 
