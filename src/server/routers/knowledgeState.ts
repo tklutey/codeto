67926647f1c1c9dec62f9bc37b5a3a8625c50fd2
tklutey <kluteyt@gmail.com>
2022-10-24 18:04:13 -0500

    Removing dummy knowledgeState.get API

--- src/server/routers/knowledgeState.ts
+++ src/server/routers/knowledgeState.ts
@@ -1,18 +1,6 @@
 import * as trpc from '@trpc/server';
 import SbClient from 'server/client/SbClient';
-import { definitions } from 'types/supabase';
 import { z } from 'zod';
-
-const generateFakeMastery = (learningStandard: definitions['decorated_denormalized_standards']) => {
-  if (learningStandard.standard_code) {
-    const mastery = Math.random() * 100;
-    return {
-      ...learningStandard,
-      mastery
-    };
-  }
-  return learningStandard;
-};
 
 const getCourseSummary = async () => {
   const sbClient = new SbClient();
@@ -68,27 +56,6 @@ const getCourseSummary = async () => {
 
 export const knowledgeState = trpc
   .router()
-  .query('get', {
-    async resolve() {
-      const sbClient = new SbClient();
-      const learningStandards = await sbClient.getKnowledgeState();
-      const masteryMappedStandards = learningStandards?.map(generateFakeMastery);
-      // @ts-ignore
-      const units = [...new Set(masteryMappedStandards?.map((ls) => ls.unit_id))];
-      const a = units.map((unit) => {
-        const matchingStandards = masteryMappedStandards?.filter((standard) => standard.unit_id === unit);
-        // @ts-ignore
-        const unitMastery = matchingStandards?.reduce((acc, standard) => acc + standard.mastery, 0) / matchingStandards?.length;
-        return {
-          unit_id: unit,
-          unit_name: matchingStandards?.[0].unit_name,
-          unit_mastery: unitMastery ? Math.round(unitMastery) : 0,
-          standards: matchingStandards
-        };
-      });
-      return a;
-    }
-  })
   .mutation('update', {
     input: z.object({
       learningStandards: z.array(z.number()),
