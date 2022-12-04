32412a3da49dc0322097f350932551e7f8ba4e62
tklutey <kluteyt@gmail.com>
2022-12-04 11:17:23 -0600

    Adding page to sandbox

--- src/pages/student/sandbox.tsx
+++ src/pages/student/sandbox.tsx
@@ -2,13 +2,16 @@ import Layout from 'layout';
 import { ReactElement, useState } from 'react';
 import { Box } from '@mui/material';
 import IDE from 'components/ide';
+import Page from 'ui-component/Page';
 
 const Sandbox = () => {
   const [userCode, setUserCode] = useState<string | undefined>('');
   return (
-    <Box height={'100%'} width={'100%'}>
-      <IDE width={'100%'} height={'100%'} language={'java'} userCode={userCode} setUserCode={setUserCode} />
-    </Box>
+    <Page title={'Sandbox'}>
+      <Box height={'100%'} width={'100%'}>
+        <IDE width={'100%'} height={'100%'} language={'java'} userCode={userCode} setUserCode={setUserCode} />
+      </Box>
+    </Page>
   );
 };
 
