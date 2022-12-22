a0a9f93720b2b5223c5b8f23a48fd37d2f33f707
tklutey <kluteyt@gmail.com>
2022-12-22 09:58:52 -0800

    Basic text box for GPT question

--- src/components/assignment/GetUnstuckModal.tsx
+++ src/components/assignment/GetUnstuckModal.tsx
@@ -1,7 +1,7 @@
 import Modal from '@mui/material/Modal';
-import { Box, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import { useTheme } from '@mui/styles';
+import { Box, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, Typography } from '@mui/material';
+import React from 'react';
+import SendIcon from '@mui/icons-material/Send';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -18,35 +18,36 @@ const style = {
 
 const GetUnstuckModal = (props: Props) => {
   const { isOpen, handleClose, youtubeTutorialUrl } = props;
-  const [isUserCodeCorrect, setIsUserCodeCorrect] = useState(false);
-  const theme = useTheme();
-  const goToNextProblem = () => {
-    handleClose();
-  };
 
-  const updateCode = (newCode?: string, _?: any) => {
-    if (newCode !== undefined) {
-      setUserCode(newCode);
-      if (newCode === solutionCode) {
-        setIsUserCodeCorrect(true);
-      } else {
-        setIsUserCodeCorrect(false);
-      }
-    }
-  };
   return (
     <Modal open={isOpen} onClose={handleClose}>
       <Box sx={style}>
         <Typography variant="h2">Get Unstuck</Typography>
-        <iframe
-          src={youtubeTutorialUrl}
-          width="45%"
-          height="90%"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        <Box display={'flex'} height={'100%'}>
+          <iframe
+            src={youtubeTutorialUrl}
+            width="45%"
+            height="90%"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <FormControl sx={{ m: 1, width: '50%' }}>
+            <InputLabel htmlFor="outlined-adornment-amount">Ask a question</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={() => console.log('click')}>
+                    <SendIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Amount"
+            />
+          </FormControl>
+        </Box>
       </Box>
     </Modal>
   );
