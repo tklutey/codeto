045d25261708a0414af601b5092c98f44c4cd377
tklutey <kluteyt@gmail.com>
2022-12-23 14:57:31 -0800

    Fixing IDE divider double render

--- src/components/ide/IDEDivider.tsx
+++ src/components/ide/IDEDivider.tsx
@@ -2,9 +2,8 @@ import { Divider } from '@mui/material';
 import { useTheme } from '@mui/styles';
 
 const IDEDivider = () => {
-  console.log('render');
   const theme = useTheme();
-  const width = 10;
+  const width = 5;
   const margin = (width / 2) * -1;
   return (
     <Divider
@@ -14,7 +13,8 @@ const IDEDivider = () => {
         backgroundColor: theme.palette.primary.main,
         marginLeft: `${margin}px`,
         opacity: 1,
-        borderWidth: 0
+        borderWidth: 0,
+        zIndex: 1
       }}
     />
   );
