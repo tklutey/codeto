15739a9b4deccfb391a282ff8a04012f2821d198
Tucker Klutey <kluteyt@gmail.com>
2022-12-26 21:43:45 -0800

    Making walkthrough viewable/opaque with a click

--- src/components/assignment/AssignmentWalkthrough.tsx
+++ src/components/assignment/AssignmentWalkthrough.tsx
@@ -1,8 +1,7 @@
-import { Alert, Box, Button, Typography } from '@mui/material';
+import { Alert, Box, Button, Typography, Tooltip } from '@mui/material';
 import ThemedDiffEditor from 'components/ide/editor/ThemedDiffEditor';
 import React, { useState } from 'react';
 import { useTheme } from '@mui/styles';
-import { trpc } from '../../utils/trpc';
 
 const AssignmentWalkthrough = ({
   handleTestCode,
@@ -35,54 +34,65 @@ const AssignmentWalkthrough = ({
   };
 
   const SolutionContent = (
-    <Box height={'100%'}>
-      <Alert severity="info" sx={{ marginY: '10px' }}>
-        Modify your code on the right to match the solution code.
-      </Alert>
-      <Box sx={{ display: 'flex', paddingBottom: '10px' }}>
-        <Box sx={{ width: '50%' }}>
-          <Typography variant="h4">Solution</Typography>
-        </Box>
-        <Box sx={{ width: '50%' }}>
-          <Typography variant="h4">Your Code</Typography>
-        </Box>
-      </Box>
-      <ThemedDiffEditor language={language} originalCode={solutionCode} modifiedCode={userCode} updateCode={updateCode} />
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <Button
-          variant="contained"
-          disabled={!isUserCodeCorrect}
-          sx={{
-            marginTop: '8px',
-            color: theme.palette.common.black,
-            background: theme.palette.warning.dark,
-            '&:hover': { background: theme.palette.warning.main }
-          }}
-          onClick={handleTestCode}
+    <Box height={'100%'} sx={{ position: 'relative' }}>
+      {!showSolution && (
+        <Box
+          height={'100%'}
+          width={'100%'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
+          sx={{ position: 'absolute', zIndex: 1 }}
         >
-          Test Your Solution
-        </Button>
+          <Tooltip title="Viewing the walkthrough will lower your mastery for this skill.">
+            <Button
+              variant="contained"
+              sx={{
+                marginTop: '8px',
+                color: theme.palette.common.black,
+                background: theme.palette.warning.dark,
+                '&:hover': { background: theme.palette.warning.main }
+              }}
+              onClick={handleShowSolution}
+            >
+              View Walkthrough
+            </Button>
+          </Tooltip>
+        </Box>
+      )}
+      <Box height={'100%'} sx={{ position: 'relative', filter: !showSolution ? 'blur(5px)' : null }}>
+        <Alert severity="info" sx={{ marginY: '10px' }}>
+          Modify your code on the right to match the solution code.
+        </Alert>
+        <Box sx={{ display: 'flex', paddingBottom: '10px' }}>
+          <Box sx={{ width: '50%' }}>
+            <Typography variant="h4">Solution</Typography>
+          </Box>
+          <Box sx={{ width: '50%' }}>
+            <Typography variant="h4">Your Code</Typography>
+          </Box>
+        </Box>
+        <ThemedDiffEditor language={language} originalCode={solutionCode} modifiedCode={userCode} updateCode={updateCode} />
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button
+            variant="contained"
+            disabled={!isUserCodeCorrect}
+            sx={{
+              marginTop: '8px',
+              color: theme.palette.common.black,
+              background: theme.palette.warning.dark,
+              '&:hover': { background: theme.palette.warning.main }
+            }}
+            onClick={handleTestCode}
+          >
+            Test Your Solution
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
 
-  const BlockedSolution = (
-    <Box height={'100%'}>
-      <Button
-        variant="contained"
-        sx={{
-          marginTop: '8px',
-          color: theme.palette.common.black,
-          background: theme.palette.warning.dark,
-          '&:hover': { background: theme.palette.warning.main }
-        }}
-        onClick={handleShowSolution}
-      >
-        View Solution
-      </Button>
-    </Box>
-  );
-  return <Box height={'100%'}>{showSolution ? SolutionContent : BlockedSolution}</Box>;
+  return <Box height={'100%'}>{SolutionContent}</Box>;
 };
 
 type Props = {
