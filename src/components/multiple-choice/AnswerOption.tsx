693d394effe0f2a2e54fb9bb9de1180edfe8720f
Tucker Klutey <kluteyt@gmail.com>
2023-01-07 09:28:08 -0600

    Add answer selecting UX to MC question

--- src/components/multiple-choice/AnswerOption.tsx
+++ src/components/multiple-choice/AnswerOption.tsx
@@ -1,9 +1,9 @@
 import { Button } from '@mui/material';
 import React from 'react';
 
-const AnswerOption = ({ optionText }: Props) => {
+const AnswerOption = ({ optionText, onClick, isSelected }: Props) => {
   return (
-    <Button variant={'outlined'} sx={{ marginY: '3px' }}>
+    <Button variant={isSelected ? 'contained' : 'outlined'} sx={{ marginY: '3px' }} onClick={onClick}>
       {optionText}
     </Button>
   );
@@ -11,5 +11,7 @@ const AnswerOption = ({ optionText }: Props) => {
 
 type Props = {
   optionText: string;
+  onClick: () => void;
+  isSelected: boolean;
 };
 export default AnswerOption;
--- src/components/multiple-choice/Question.tsx
+++ src/components/multiple-choice/Question.tsx
@@ -1,14 +1,19 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import AnswerOption from './AnswerOption';
 
 const Question = ({ prompt, answerOptions }: Props) => {
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null);
   return (
-    <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
-      <Typography variant={'body2'}>{prompt}</Typography>
-      <Box display={'flex'} flexDirection={'column'} width={'50%'}>
+    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} width={'50%'}>
+      <Box width={'100%'}>
+        <Typography variant={'h3'} sx={{ marginY: '10px' }}>
+          {prompt}
+        </Typography>
+      </Box>
+      <Box display={'flex'} flexDirection={'column'} width={'100%'}>
         {answerOptions.map((option, index) => (
-          <AnswerOption key={index} optionText={option} />
+          <AnswerOption key={index} optionText={option} onClick={() => setSelectedAnswer(index)} isSelected={index === selectedAnswer} />
         ))}
       </Box>
     </Box>
